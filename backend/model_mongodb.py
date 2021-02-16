eedeaef143cd9e6d19ceea526342f6486154e1c7
andrewdoud <andrewxdoud@gmail.com>
2021-02-15 22:43:53 -0800

    using Fernando's backend and Matt's frontend

---
+++ backend/model_mongodb.py
@@ -0,0 +1,50 @@
+import pymongo
+from bson import ObjectId
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+class Inventory(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)
+    collection = db_client["inventory"]["products_list"]
+
+    def find_all(self):
+        products = list(self.collection.find())
+        for product in products:
+            product["_id"] = str(product["_id"])
+        return products
+
+    def find_by_name(self, name):
+        products = list(self.collection.find({"name": name}))
+        for product in products:
+            product["_id"] = str(product["_id"])
+        return products
+        
