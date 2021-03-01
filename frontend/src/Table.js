282ac7f19356bec5a4e42c462c38953730aa2be5
andrewdoud <andrewxdoud@gmail.com>
2021-03-01 09:53:21 -0800

    Codepen frontend template initial commit

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -33,21 +33,21 @@ const TableBody = props => {
             <button onClick={() => props.removeCharacter(index)}>Delete</button>
          </td>
       </tr>
-     )
-   })
+     );
+   });
 
    return <tbody>{rows}</tbody>
 }
 
 const Table = props => {
-   const { characterData, removeCharacter } = props
+   const { characterData, removeCharacter } = props;
 
    return (
       <table>
          <TableHeader />
          <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
-   )
+   );
 }
 
 export default Table
