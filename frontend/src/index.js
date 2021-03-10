1c482000081351cb4faf592ce6c5795da1f525e8
andrewdoud <andrewxdoud@gmail.com>
2021-03-09 22:46:47 -0800

    please please work

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import './index.css';
+import './standard.css'
+import './index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
